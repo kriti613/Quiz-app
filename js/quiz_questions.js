fd66918f4bbaa31bb53e00b67b184167e3bfdbab
Kriti Gupta <kg26071999@gmail.com>
2021-04-28 13:31:14 +0530

    created html css and Javascript file
    code

---
+++ js/quiz_questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+ {
+    q: 'Name of the screen that recognizes touch input is :',
+    option:['Recog screen','Point Screen','Touch Screen','Android Screen'],
+    answer:2
+ },
+ {
+    q: 'Identify the device through which data and instructions are entered into a computer',
+    option:['Software','Output device','Input device','Memory'],
+    answer:2
+ },
+ {
+    q:'The output shown on the computer monitor is called',
+    option:['Soft Copy','VDU','Hard Copy','Screen Copy'],
+    answer:0
+ },
+ {
+    q:'The printed output from a computer is called',
+    option:['Soft Copy','Paper','Copy','Hard Copy'],
+    answer:3
+ },
+ {
+    q:'Eight Bits make up a',
+    option:['Kilobyte','Byte','Megabyte','None'],
+    answer:1
+ },
+{
+    q:'Arrange in ascending order the units of memory TB, KB, GB, MB',
+    option:['TB>MB>GB>KB','MB>GB>TB>KB','TB>GB>MB>KB','GB>MB>KB>TB'],
+    answer:2
+},
+{
+    q:'Computer Moniter is also known as :',
+    option:['DVU','VDU','UVD','CCTV'],
+    answer:1
+},
+{
+    q:'Which one is the result of the output given by a computer',
+    option:['Data','Istruction','Information','Excursion'],
+    answer:2
+},
+{
+    q:'Which one of these also known as read/write memory ?',
+    option:['ROM','RAM','DVD','Hard Disk'],
+    answer:1
+},
+{
+    q:'The printed output from a computer is called',
+    option:['Copy','Soft Copy','Hard Copy','Paper'],
+    answer:2
+}
+]
